21a3ccfc6f60a667031ef66d6dfa20a91d43edf2
K <kalnajar09@gmail.com>
2013-03-29 07:32:39 -0400

    setup initial views and urls

--- ResOut/urls.py
+++ ResOut/urls.py
@@ -4,16 +4,31 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ResOut.views.home', name='home'),
-    # url(r'^ResOut/', include('ResOut.foo.urls')),
-
-    (r'^grappelli/', include('grappelli.urls')),
+urlpatterns = patterns('ResOut',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('ApiApp.views',
+
+	#url(r'^api/token/$', 'EmptyTokenCall'),
+
+	url(r'^api/reservation/camps/(?P<res_id>\d{1,10})/$', 'ReservationCamps'),
+	url(r'^api/reservation/documents/(?P<res_id>\d{1,10})/$', 'ReservationDocuments'),
+	url(r'^api/reservation/contacts/(?P<res_id>\d{1,10})/$', 'ReservationContacts'),
+
+	url(r'^api/camp/areas/(?P<camp_id>\d{1,10})/$', 'CampAreas'),
+	url(r'^api/camp/contacts/(?P<camp_id>\d{1,10})/$', 'CampContacts'),
+	url(r'^api/camp/documents/(?P<camp_id>\d{1,10})/$', 'CampDocuments'),
+	url(r'^api/camp/ranks/(?P<camp_id>\d{1,10})/$', 'CampRanks'),
+        url(r'^api/camp/staff/(?P<camp_id>\d{1,10})/$', 'CampStaff'),
+)
+
+urlpatterns += patterns('',
+    (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/admin/'}),
+    (r'^grappelli/', include('grappelli.urls')),
 )
--- api_app/views.py
+++ api_app/views.py
@@ -1,1 +1,68 @@
-# Create your views here.
+#resout/api_app
+
+#python helpers
+import json
+from datetime import datetime
+from decimal import *
+
+#django view helpers
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import redirect
+
+#django models
+from django.db.models.loading import get_model
+from django.db.models import Q
+from django.forms.models import model_to_dict
+from django.db import models
+
+#django authentication
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+#custom imports
+from reservations_app.models import *
+from camps_app.models import *
+
+def SerializeResponse(response_data):
+    json_serializer = serializers.get_serializer("json")()
+    response = json_serializer.serialize(response_data, ensure_ascii=False)
+    return HttpResponse(response, mimetype="application/json")
+
+def ReservationCamps(request, res_id):
+    response_data = ReservationCamp.objects.filter(reservation=res_id)
+    return SerializeResponse(response_data)
+
+def ReservationDocuments(request, res_id):
+    response_data = ReservationDocument.objects.filter(reservation=res_id)
+    return SerializeResponse(response_data)
+
+def ReservationContacts(request, res_id):
+    response_data = ReservationContact.objects.filter(reservation=res_id)
+    return SerializeResponse(response_data)
+
+def CampAreas(request, camp_id):
+    response_data = CampArea.objects.filter(camp=camp_id)
+    return SerializeResponse(response_data)
+
+def CampContacts(request, camp_id):
+    response_data = CampContact.objects.filter(camp=camp_id)
+    return SerializeResponse(response_data)
+
+def CampDocuments(request, camp_id):
+    response_data = CampDocument.objects.filter(camp=camp_id)
+    return SerializeResponse(response_data)
+
+def CampRanks(request, camp_id):
+    response_data = CampRank.objects.filter(camp=camp_id)
+    return SerializeResponse(response_data)
+
+def CampStaff(request, camp_id):
+    response_data = CampStaff.objects.filter(camp=camp_id)
+    return SerializeResponse(response_data)
