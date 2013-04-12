62e80c2a2e501e126d3a9d5789aa404b9ec026cc
Kinetic Alnajar <kalnajar09@gmail.com>
2013-04-12 16:34:48 -0400

    working on the admin fields for custom admin model #5

--- resout_app/admin.py
+++ resout_app/admin.py
@@ -8,15 +8,15 @@ from django.contrib.auth.forms import UserChangeForm
 class MyUserChangeForm(UserChangeForm):
 	class Meta:#(UserChangeForm.Meta):
 		model = ReservationAdminUser2
-		fields = ('username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'is_reservation_admin2')
+		fields = ('username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'is_reservation_admin2', 'is_staff')
 
 class MyUserAdmin(UserAdmin):
 	form = MyUserChangeForm
 	#fields = ('username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'is_reservation_admin2')
 
-	fieldsets = UserAdmin.fieldsets + (
-		(None, {'fields': ('is_reservation_admin2',)}),
-	)
+	#fieldsets = UserAdmin.fieldsets + (
+		#(None, {'fields': ('is_reservation_admin2',)}),
+	#)
 
 
 admin.site.register(ReservationAdminUser2, MyUserAdmin)
