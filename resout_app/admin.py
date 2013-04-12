fda70a5ed8a922d78b05f381aa233b6a9cb04eec
Kinetic Alnajar <kalnajar09@gmail.com>
2013-04-12 16:23:03 -0400

    moving the fields to the form

--- resout_app/admin.py
+++ resout_app/admin.py
@@ -7,11 +7,12 @@ from django.contrib.auth.forms import UserChangeForm
 
 class MyUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
-        model = ReservationAdminUser2
+		model = ReservationAdminUser2
+		fields =  ('username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'is_reservation_admin2')
 
 class MyUserAdmin(UserAdmin):
 	form = MyUserChangeForm
-	exclude = ('username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'is_reservation_admin2')
+	#exclude = ('username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'is_reservation_admin2')
 
 	fieldsets = UserAdmin.fieldsets + (
 		(None, {'fields': ('is_reservation_admin2',)}),
