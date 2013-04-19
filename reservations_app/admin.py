f2e8d1c3402843001e3dea58a9927340f5a77d21
K <kalnajar09@gmail.com>
2013-04-19 05:32:00 -0400

    started modifying admin to restrict to reservation for all reservation models

--- reservations_app/admin.py
+++ reservations_app/admin.py
@@ -34,17 +34,18 @@ class FilterUserAdmin(admin.ModelAdmin):
 						# return True
 		# return obj.user == request.user
 
-	# def formfield_for_foreignkey(self, db_field, request, **kwargs):
-		# if db_field.name == "venue" and not request.user.is_superuser:
-				# kwargs["queryset"] = Venue.objects.filter(venue_owner=request.user)
-				# return db_field.formfield(**kwargs)
+	def formfield_for_foreignkey(self, db_field, request, **kwargs):
+		if db_field.name == "type" and not request.user.is_superuser:
+                                res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
+				kwargs["queryset"] = ReservationDocumentType.objects.filter(reservation=res_admin.reservation)
+				return db_field.formfield(**kwargs)
 		# if db_field.name == "bar" and not request.user.is_superuser:
 				# kwargs["queryset"] = VenueBar.objects.filter(user=request.user)
 				# return db_field.formfield(**kwargs)
 		# if db_field.name == "drink_type" and not request.user.is_superuser:
 				# kwargs["queryset"] = DrinkType.objects.filter(user=request.user)
 				# return db_field.formfield(**kwargs)
-		# return super(FilterUserAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+		return super(FilterUserAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 # # ===================================================#
 
 # class ReservationSiteAdministratorAdmin(FilterUserAdmin):
@@ -53,17 +54,17 @@ class FilterUserAdmin(admin.ModelAdmin):
 class ReservationCampAdmin(FilterUserAdmin):
 	exclude = ('user',)
 	
-# class ReservationDocumentTypeAdmin(FilterUserAdmin):
-	# exclude = ('user', 'reservation')
+class ReservationDocumentTypeAdmin(FilterUserAdmin):
+	exclude = ('user',)
 	
-# class ReservationDocumentAdmin(FilterUserAdmin):
-	# exclude = ('user', 'reservation')
+class ReservationDocumentAdmin(FilterUserAdmin):
+	exclude = ('user',)
 	
-# class ReservationContactAdmin(FilterUserAdmin):
-	# exclude = ('user', 'reservation')
+class ReservationContactAdmin(FilterUserAdmin):
+	exclude = ('user',)
 
 #admin.site.register(ReservationSiteAdministrator)
 admin.site.register(ReservationCamp, ReservationCampAdmin)
-admin.site.register(ReservationDocumentType)
-admin.site.register(ReservationDocument)
-admin.site.register(ReservationContact)
+admin.site.register(ReservationDocumentType, ReservationDocumentTypeAdmin)
+admin.site.register(ReservationDocument, ReservationDocumentAdmin)
+admin.site.register(ReservationContact, ReservationContactAdmin)
