a7e9564905ab0589a60a2d51ca747f547458b830
K <kalnajar09@gmail.com>
2013-04-19 04:15:48 -0400

    import UserCreationForm

--- resout_app/admin.py
+++ resout_app/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from resout_app.models import *
 
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 class CampAdminUserChangeForm(UserChangeForm):
 	class Meta:#(UserChangeForm.Meta):
