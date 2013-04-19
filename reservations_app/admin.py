56e5315c0d4207160ac97ebccaeeb18a784aae05
K <kalnajar09@gmail.com>
2013-04-19 05:26:53 -0400

    started modifying admin to restrict to reservation for camps

--- reservations_app/admin.py
+++ reservations_app/admin.py
@@ -1,9 +1,10 @@
 #resout/reservations_app
 from django.contrib import admin
+from resout_app.models import *
 from reservations_app.models import *
 
 # # ===================================================#
-# class FilterUserAdmin(admin.ModelAdmin):
+class FilterUserAdmin(admin.ModelAdmin):
 
 	# def save_model(self, request, obj, form, change):
 		# try:
@@ -13,11 +14,17 @@ from reservations_app.models import *
 				
 		# obj.save()
 
-	# def queryset(self, request): 
-		# qs = super(FilterUserAdmin, self).queryset(request)
-		# if request.user.is_superuser:
-						# return qs
-		# return qs.filter(reservation=request.user)
+	def get_readonly_fields(self, request, obj=None):
+                if not request.user.is_superuser:
+                        return self.readonly_fields + ('reservation',)
+                return self.readonly_fields
+
+	def queryset(self, request): 
+		qs = super(FilterUserAdmin, self).queryset(request)
+		if request.user.is_superuser:
+                    return qs
+                res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
+		return qs.filter(reservation=res_admin.reservation)
 
 	# def has_change_permission(self, request, obj=None):
 		# if not obj:
@@ -43,8 +50,8 @@ from reservations_app.models import *
 # class ReservationSiteAdministratorAdmin(FilterUserAdmin):
 	# exclude = ('user', 'reservation')
 
-# class ReservationCampAdmin(FilterUserAdmin):
-	# exclude = ('user', 'reservation')
+class ReservationCampAdmin(FilterUserAdmin):
+	exclude = ('user',)
 	
 # class ReservationDocumentTypeAdmin(FilterUserAdmin):
 	# exclude = ('user', 'reservation')
@@ -56,7 +63,7 @@ from reservations_app.models import *
 	# exclude = ('user', 'reservation')
 
 #admin.site.register(ReservationSiteAdministrator)
-admin.site.register(ReservationCamp)
+admin.site.register(ReservationCamp, ReservationCampAdmin)
 admin.site.register(ReservationDocumentType)
 admin.site.register(ReservationDocument)
 admin.site.register(ReservationContact)
