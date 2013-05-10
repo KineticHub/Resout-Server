cb31d256f59c2f61bd2421154601666097dc8404
Kinetic Alnajar <kalnajar09@gmail.com>
2013-05-09 22:28:25 -0400

    fixed indentation issues

--- reservations_app/admin.py
+++ reservations_app/admin.py
@@ -23,15 +23,15 @@ class FilterUserAdmin(admin.ModelAdmin):
 		obj.save()
 
 	def get_readonly_fields(self, request, obj=None):
-                if not request.user.is_superuser:
-                        return self.readonly_fields + ('reservation',)
-                return self.readonly_fields
+				if not request.user.is_superuser:
+						return self.readonly_fields + ('reservation',)
+				return self.readonly_fields
 
 	def queryset(self, request): 
 		qs = super(FilterUserAdmin, self).queryset(request)
 		if request.user.is_superuser:
-                    return qs
-                res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
+					return qs
+				res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
 		return qs.filter(reservation=res_admin.reservation)
 
 	# def has_change_permission(self, request, obj=None):
@@ -44,7 +44,7 @@ class FilterUserAdmin(admin.ModelAdmin):
 
 	def formfield_for_foreignkey(self, db_field, request, **kwargs):
 		if db_field.name == "type" and not request.user.is_superuser:
-                                res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
+								res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
 				kwargs["queryset"] = ReservationDocumentType.objects.filter(reservation=res_admin.reservation)
 				return db_field.formfield(**kwargs)
 		# if db_field.name == "bar" and not request.user.is_superuser:
@@ -65,18 +65,18 @@ class ReservationCampAdmin(FilterUserAdmin):
 	def queryset(self, request): 
 		qs = super(FilterUserAdmin, self).queryset(request)
 		if request.user.is_superuser:
-                    return qs
+					return qs
 		if not request.user.is_superuser:
-                    try:
-                        res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
+					try:
+						res_admin = ReservationAdminUser.objects.get(pk=request.user.id)
 						return qs.filter(reservation=res_admin.reservation)
-                    except:
-                        try:
-                            camp_admin = CampAdminUser.objects.get(pk=request.user.id)
+					except:
+						try:
+							camp_admin = CampAdminUser.objects.get(pk=request.user.id)
 							return qs.get(pk=camp_admin.camp.pk)
-                        except:
-                            # THIS SHOULD NOT HAPPEN, BUT SMOOTH HANDLING IS GOOD
-                            pass
+						except:
+							# THIS SHOULD NOT HAPPEN, BUT SMOOTH HANDLING IS GOOD
+							pass
 	
 class ReservationDocumentTypeAdmin(FilterUserAdmin):
 	exclude = ('user',)
